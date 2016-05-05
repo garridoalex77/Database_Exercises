1dbf8e3105a6641ab94d5283ad06338f300230b1
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-05 15:33:02 -0500

    initial commit

---
+++ albums_migration.sql
@@ -0,0 +1,23 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(50) DEFAULT 'NONE',
+    release_date YEAR(4),
+    sales FLOAT,
+    genre VARCHAR(10) DEFAULT 'NONE',
+    PRIMARY KEY (id)
+
+);
+
+
+
+
+
+
+
+
+
+
