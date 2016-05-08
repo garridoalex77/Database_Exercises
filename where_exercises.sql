628f455435150eccfd7bf8cf24da1ae5575a0d05
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-08 17:39:30 -0500

    searching q not qu

--- where_exercises.sql
+++ where_exercises.sql
@@ -38,6 +38,11 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
 
+SELECT emp_no, first_name, last_name 
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
+
 
 
 
