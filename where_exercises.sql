2f24cf50ab91c1fc449c5ba98e8607dae30e373d
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-08 17:21:12 -0500

    q exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,4 @@
 USE codeup_test_db;
-SELECT emp_no, first_name, last_name, birth_date 
+SELECT emp_no, first_name, last_name 
 FROM employees
-WHERE birth_date LIKE '%%%%-12-25';
+WHERE last_name LIKE '%q%';
