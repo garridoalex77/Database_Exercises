9ee6dcaffdd82bc96a55d9bcd7e6d8f236dca737
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-08 17:36:47 -0500

    e last and first letter check

--- where_exercises.sql
+++ where_exercises.sql
@@ -19,7 +19,7 @@ WHERE last_name LIKE 'E%';
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' 
-    OR last_name LIKE '%e';
+    AND last_name LIKE '%e';
 
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
