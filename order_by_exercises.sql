a697f1a2d33248b67ba3d81a8cd26b21a1f95702
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 09:36:42 -0500

    swapped order to last, first

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -12,7 +12,7 @@ WHERE gender = 'M'
         first_name = 'Irena'
     OR first_name =  'Vidya'
     OR first_name = 'Maya')
-ORDER BY first_name, last_name;
+ORDER BY last_name, first_name;
 
 -- SELECT emp_no, first_name, last_name
 -- FROM employees
