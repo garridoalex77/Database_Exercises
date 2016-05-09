afccc07242e63ff5e0aa0de74714c2cb224e612d
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 13:28:57 -0500

    counting m and f

---
+++ functions_exercises.sql
@@ -0,0 +1,60 @@
+USE codeup_test_db;
+-- SELECT emp_no, first_name, last_name
+-- FROM employees
+-- WHERE first_name = 'Irena'
+--     OR first_name =  'Vidya'
+--     OR first_name = 'Maya'
+
+SELECT COUNT(first_name)emp_no, first_name, last_name
+FROM employees
+WHERE gender = 'M'
+    AND (
+        first_name = 'Irena'
+    OR first_name =  'Vidya'
+    OR first_name = 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT COUNT(first_name), GENDER
+FROM employees
+WHERE
+    first_name = 'Irena'
+    OR first_name =  'Vidya'
+    OR first_name = 'Maya'
+GROUP BY GENDER = 'F';
+
+-- SELECT emp_no, first_name, last_name
+-- FROM employees
+-- WHERE last_name LIKE 'E%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' 
+    AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+-- SELECT emp_no, first_name, last_name, hire_date
+-- FROM employees
+-- WHERE hire_date LIKE '199%';
+
+-- SELECT emp_no, first_name, last_name, birth_date 
+-- FROM employees
+-- WHERE birth_date LIKE '%%%%-12-25';
+
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%%%%-12-25'
+ORDER BY birth_date, hire_date DESC;
+
+-- SELECT emp_no, first_name, last_name 
+-- FROM employees
+-- WHERE last_name LIKE '%q%';
+
+SELECT emp_no, first_name, last_name 
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
+
+
+
+
