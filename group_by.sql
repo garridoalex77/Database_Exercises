49d78b9d7d2107f335e45f36e5601ead165aa6af
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 10:51:25 -0500

    distinct q not qu last names

--- group_by.sql
+++ group_by.sql
@@ -13,3 +13,9 @@ FROM employees
 WHERE last_name LIKE 'E%' 
     AND last_name LIKE '%e'
 GROUP BY first_name, last_name;
+
+SELECT emp_no, first_name, last_name 
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
