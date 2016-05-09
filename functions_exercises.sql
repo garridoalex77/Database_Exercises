70980db5c9767b6b7cf7a2932ee84e5ce3b3640f
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 13:36:17 -0500

    checking datediff

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -52,6 +52,12 @@ WHERE hire_date LIKE '199%'
     AND birth_date LIKE '%%%%-12-25'
 ORDER BY birth_date, hire_date DESC;
 
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%%%%-12-25'
+ORDER BY birth_date, hire_date DESC;
+
 -- SELECT emp_no, first_name, last_name 
 -- FROM employees
 -- WHERE last_name LIKE '%q%';
