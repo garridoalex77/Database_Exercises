8013f352920b21820c014b667cba3f7b39327249
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 11:17:58 -0500

    limiting and offsetting

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -8,3 +8,10 @@ WHERE hire_date LIKE '199%'
     AND birth_date LIKE '%%%%-12-25'
 ORDER BY birth_date, hire_date DESC
 LIMIT 5;
+
+SELECT first_name, last_name 
+FROM employees
+WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%%%%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
