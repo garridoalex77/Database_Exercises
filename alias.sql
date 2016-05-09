a7b28d8621cd0ee546ece7ca50c2303e4bff45dc
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 14:25:02 -0500

    alias exercise

---
+++ alias.sql
@@ -0,0 +1,2 @@
+SELECT CONCAT(emp_no, ' - ',last_name, ', ', first_name) 
+FROM employees LIMIT 10
--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,18 +1,4 @@
 USE codeup_test_db;
--- SELECT emp_no, first_name, last_name
--- FROM employees
--- WHERE first_name = 'Irena'
---     OR first_name =  'Vidya'
---     OR first_name = 'Maya'
-
-SELECT COUNT(first_name)emp_no, first_name, last_name
-FROM employees
-WHERE gender = 'M'
-    AND (
-        first_name = 'Irena'
-    OR first_name =  'Vidya'
-    OR first_name = 'Maya')
-ORDER BY last_name, first_name;
 
 SELECT COUNT(first_name), GENDER
 FROM employees
@@ -22,51 +8,16 @@ WHERE
     OR first_name = 'Maya'
 GROUP BY GENDER = 'F';
 
--- SELECT emp_no, first_name, last_name
--- FROM employees
--- WHERE last_name LIKE 'E%';
-
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE last_name LIKE 'E%' 
-    AND last_name LIKE '%e'
-ORDER BY emp_no DESC;
-
-SELECT emp_no, CONCAT(first_name, last_name)
-FROM employees
-WHERE last_name LIKE 'E%' 
-    AND last_name LIKE '%e'
-ORDER BY emp_no DESC;
-
--- SELECT emp_no, first_name, last_name, hire_date
--- FROM employees
--- WHERE hire_date LIKE '199%';
-
--- SELECT emp_no, first_name, last_name, birth_date 
--- FROM employees
--- WHERE birth_date LIKE '%%%%-12-25';
-
-SELECT emp_no, first_name, last_name, hire_date, birth_date
+SELECT emp_no, first_name, last_name, hire_date, birth_date, datediff(now(), hire_date)
 FROM employees
 WHERE hire_date LIKE '199%'
     AND birth_date LIKE '%%%%-12-25'
 ORDER BY birth_date, hire_date DESC;
 
-SELECT emp_no, first_name, last_name, hire_date, birth_date
-FROM employees
-WHERE hire_date LIKE '199%'
-    AND birth_date LIKE '%%%%-12-25'
-ORDER BY birth_date, hire_date DESC;
-
--- SELECT emp_no, first_name, last_name 
--- FROM employees
--- WHERE last_name LIKE '%q%';
-
-SELECT emp_no, first_name, last_name 
+SELECT emp_no, first_name, last_name, COUNT(last_name)
 FROM employees
 WHERE last_name LIKE '%q%'
-    AND last_name NOT LIKE '%qu%';
-
-
-
+    AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name
+ORDER BY COUNT(first_name) DESC;
 
