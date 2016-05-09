d6d3217bf47cb4720e61ad6b77f0b514b9ef30e5
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-09 11:15:31 -0500

    ordering and limiting to 5

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,10 @@
 USE codeup_test_db;
 
 SELECT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
+
+SELECT first_name, last_name 
+FROM employees
+WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%%%%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
